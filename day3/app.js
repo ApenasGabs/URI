fbf0ae9f604892ff55d1a91afc9ef90c0d47cee5
ApenasGabs <gabers357@gmail.com>
2021-03-22 22:02:45 -0300

    the right way to solve

--- day3/app.js
+++ day3/app.js
@@ -1,7 +1,9 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
-var pi = 3.14159
-var raio2 = Math.pow(parseFloat(lines), 2)
-var area = pi* raio2
-console.log("A="+ area);
+var PI = 3.14159;
+var raio = parseFloat(input);
+
+var area = PI * (raio*raio)
+
+console.log("A=" + area.toFixed(4));
