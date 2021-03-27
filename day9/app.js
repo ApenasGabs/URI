e8c692f6bbe724bac7d9d5c21db9931a522bbb20
ApenasGabs <gabers357@gmail.com>
2021-03-26 23:25:37 -0300

    day 9 solved

--- day9/app.js
+++ day9/app.js
@@ -1,3 +1,12 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
+
+var NUMBER = lines.shift();
+var time = parseInt(lines.shift());
+var money = parseFloat(lines.shift());
+
+var SALARY = (time*money);
+
+console.log( "NUMBER =",NUMBER,);
+console.log("SALARY = U$",SALARY.toFixed(2));
