e9c0850ecc5e8041a0881a0f94d049ac036b58b7
Apenasgabs <gabers357@gmail.com>
2021-03-29 13:47:17 -0300

    day10 finished

--- day10/app.js
+++ day10/app.js
@@ -6,3 +6,9 @@ var lines = input.split('\n');
 // o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). 
 // Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas,
 //  informar o total a receber no final do mês, com duas casas decimais.
+
+var nome = lines.shift();
+var salario  = parseInt(lines.shift());
+var vendas = parseFloat(lines.shift());
+var total = vendas*0.15+salario
+console.log("TOTAL = R$",total.toFixed(2));
