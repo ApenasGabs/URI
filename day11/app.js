d9d63fbf96fe256565c16e8093ad70e0ea94a311
Apenasgabs <gabers357@gmail.com>
2021-03-30 17:37:57 -0300

    day11 still not solved

--- day11/app.js
+++ day11/app.js
@@ -1,2 +1,11 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
+
+var item1 = lines.shift().split(" ");
+var item2 = lines.shift().split(" ");
+
+
+var total = 0
+
+
+console.log("VALOR A PAGAR: R$",lines);
