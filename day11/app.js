feef1981d9faf91bff2eb65333ad9760f4f54347
Apenasgabs <gabers357@gmail.com>
2021-04-05 14:37:07 -0300

    day 11 finished

--- day11/app.js
+++ day11/app.js
@@ -1,11 +1,14 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
-var item1 = lines.shift().split(" ");
-var item2 = lines.shift().split(" ");
+var array1 = lines.shift().split(" ");
+var array2 = lines.shift().split(" ");
+console.log(array1[2])
 
+item1 = array1[2] * array1[1]
+item2 = array2[2] * array2[1]
+console.log(item1)
 
-var total = 0
+var total = item2+ item1
 
-
-console.log("VALOR A PAGAR: R$",lines);
+console.log("VALOR A PAGAR: R$",total.toFixed(2));
--- day7/app.js
+++ day7/app.js
@@ -1,2 +1,11 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
+
+var a = (parseFloat(lines.shift()))/10*2;
+var b = (parseFloat(lines.shift()))/10*3;
+var c = (parseFloat(lines.shift()))/10*5;
+
+
+MEDIA = (a + b + c)/1
+
+console.log("MEDIA =", MEDIA.toFixed(1));
