844e8f6d5df28a5551f3d288a0f7d8db23ac84e8
ApenasGabs <gabers357@gmail.com>
2021-04-12 22:02:33 -0300

    😥

--- day15/app.js
+++ day15/app.js
@@ -1,4 +1,5 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var [X, Y] = input.split('\n').map(item => parseFloat(item));
+
 total = (X / Y).toFixed(3)
 console.log(total+" km/l");
--- day2/app.js
+++ day2/app.js
@@ -7,7 +7,7 @@ var lines = input.split('\n');
 // parseInt é um conversor para int assim como parseFloat e para quebrados
 var A = parseInt(lines.shift());
 var B = parseInt(lines.shift());
-
+// .shift() remove do Array
 var X = A + B
-
+console.log(typeof lines);
 console.log("X =",X);
