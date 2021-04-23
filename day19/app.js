037078bce95e1fd2930b3a0d45297f0c4fdc30e9
Apenasgabs <gabers357@gmail.com>
2021-04-22 22:53:37 -0300

    😎

---
+++ day19/app.js
@@ -0,0 +1,5 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+let valor = parseInt(input);
+
+const qtdNotas = parseInt(valor/100)
+console.log();
--- day21/app.js
+++ day21/app.js
@@ -1,7 +1,7 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
-var totsec = input.split('\n');
-
-var hr = parseInt(totsec/3600)
+var totsec2 = input.split();
+var totsec = parseInt( totsec2.shift()) 
+var hr = parseInt(totsec/3600);
 var Tmin = parseInt(totsec%3600)
 var min = parseInt(Tmin/60)
 var sec = parseInt(Tmin%60)
